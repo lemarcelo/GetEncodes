7f50ad0c78a3062c0942188521eca9d8600b5f01
Marcelo P. Santos <lemarcelo@outlook.com>
2020-07-28 23:33:36 -0300

    Guid

--- GetEncodes/GetEncodes/MainPage.xaml.cs
+++ GetEncodes/GetEncodes/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GetEncodes.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ namespace GetEncodes
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
         }
     }
 }
---
+++ GetEncodes/GetEncodes/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CEC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace GetEncodes.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _GetBytes;
+
+        public string GetBytes
+        {
+            get { return _GetBytes; }
+            set
+            {
+                _GetBytes = value;
+                Notify("GetBytes");
+            }
+        }
+        private string _ResultBytes;
+
+        public string ResultBytes
+        {
+            get { return _ResultBytes; }
+            set
+            {
+                _ResultBytes = value;
+                Notify("ResultBytes");
+            }
+        }
+        private string _GetID;
+
+        public string GetID
+        {
+            get { return _GetID; }
+            set
+            {
+                _GetID = value;
+                Notify("GetID");
+            }
+        }
+        private string _GetSHA;
+
+        public string GetSHA
+        {
+            get { return _GetSHA; }
+            set
+            {
+                _GetSHA = value;
+                Notify("GetSHA");
+            }
+        }
+
+        public ICommand GetIDCommand => new Command<string>(async (strParam) => GetIDAsync(strParam));
+
+        private void GetIDAsync(string strParam)
+        {
+            GetID = Guid.NewGuid().ToString();
+        }
+
+
+    }
+}
